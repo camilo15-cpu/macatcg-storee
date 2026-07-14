7d23e7c1dff8864432c00d47c97081d3fbfa2b79
Matias Urrutia <270664754+Matyhb99@users.noreply.github.com>
2026-07-14 16:37:31 -0400

    modificacion

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import User from '../models/user.js'; 
+import User from '../models/user.js'; // correccion linux
 const router = express.Router();
 
 // Ruta para REGISTRAR
